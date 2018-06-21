a4241358838644e0b9cf55c554e505c82444e2bc
Chao Fan <fanchao05@gmail.com>
2018-06-21 17:45:15 -0400

    setup client side

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: './client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app/assets/javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
+};
