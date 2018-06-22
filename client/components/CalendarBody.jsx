f161772cf1e26e8ffbb262abfcf7fe4b752408c2
Chao Fan <fanchao05@gmail.com>
2018-06-21 23:55:54 -0400

    added calendar rendering method

--- client/components/CalendarBody.jsx
+++ client/components/CalendarBody.jsx
@@ -4,8 +4,23 @@ class CalendarBody extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      months: [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+      ],
       days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-      date: new Date()
+      date: new Date(),
+      dateRendered: 1
     };
   }
 
@@ -15,21 +30,39 @@ class CalendarBody extends Component {
     });
   }
 
+  renderFirstRow(startDay) {
+    let count = 1;
+    let dates = [];
+    for (let i = 0; i <= 6; i++) {
+      if (i < startDay) {
+        dates[i] = '';
+      } else {
+        dates[i] = count;
+        count++;
+      }
+    }
+
+    this.setState({ dateRendered: count });
+
+    return dates.map(date => {
+      return <td key={date}>{date}</td>;
+    });
+  }
+
   render() {
+    let firstDayInMonth = `${
+      this.state.months[this.state.date.getMonth()]
+    } 1 ${this.state.date.getFullYear()}`;
+    const dateString = new Date(firstDayInMonth).toDateString();
+    const firstDay = dateString.substring(0, 3);
+    const startDay = this.state.days.indexOf(firstDay);
+
     return (
       <div className="calendar-body">
         <table>
           <tbody>
             <tr>{this.getdays()}</tr>
-            <tr>
-              <td>Jill</td>
-              <td>Smith</td>
-              <td>50</td>
-              <td>50</td>
-              <td>50</td>
-              <td>50</td>
-              <td>50</td>
-            </tr>
+            <tr>{this.renderFirstRow(startDay)}</tr>
             <tr>
               <td>Eve</td>
               <td>Jackson</td>
