e88d6a1e127407a8fc3feb2761c65f65aaea0c0a
Chao Fan <fanchao05@gmail.com>
2018-06-22 23:56:54 -0400

    added daycard and eventform components

--- client/components/DayCard.jsx
+++ client/components/DayCard.jsx
@@ -3,9 +3,23 @@ import React, { Component } from 'react';
 class DayCard extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      showForm: false
+    };
   }
+
+  renderForm() {
+    console.log('lalala');
+  }
+
   render() {
-    return <div className="day-card">{this.props.date}</div>;
+    return (
+      <div className="day-card" onDoubleClick={() => this.renderForm()}>
+        <div>{this.props.date}</div>
+        <div>double click to add event</div>
+      </div>
+    );
   }
 }
 
--- client/components/EventForm.jsx
+++ client/components/EventForm.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 
 class EventForm extends Component {
+  renderForm() {}
+
   render() {
-    return <div className="event-form" />;
+    return <div className="event-form">lalala</div>;
   }
 }
 
