2a16bc6b15848ba75f2ac10588b13b1435f1beda
Chao Fan <fanchao05@gmail.com>
2018-07-18 23:32:58 -0400

    updated code

--- client/components/CalendarHeader.jsx
+++ client/components/CalendarHeader.jsx
@@ -5,7 +5,6 @@ import { changeMonth } from '../actions/calendar_actions';
 class CalendarHeader extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       months: [
         'January',
